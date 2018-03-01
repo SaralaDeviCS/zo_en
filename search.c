5138674c9f619527324890b6f815bbfa0411d975
SaralaDeviCS <32431208+SaralaDeviCS@users.noreply.github.com>
2018-03-01 22:00:56 +0530

    Create search.c

---
+++ search.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	int a[10000];
+	printf("enter the number to be searched  ");
+	scanf("%d",&n);
+	int num,i=0,pos=-1;
+	char end;
+	while(scanf("%d%c",&a[i],&end))
+	{
+		if(a[i]==n)
+		{
+			pos=i;
+		}
+		i++;
+		if(end=='\n')
+		{
+			break;
+		}
+	}
+	if(pos==-1)
+	{
+		printf("the number is not found \n");
+		
+	}
+	else{
+	printf("%d is found in %d position",n,pos+1);
+	}
+}
