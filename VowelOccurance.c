5516e32965992742373179be94033c3579cc37fa
SaralaDeviCS <32431208+SaralaDeviCS@users.noreply.github.com>
2018-03-04 13:27:36 +0530

    Create VowelOccurance.c

---
+++ VowelOccurance.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int search(char s[1000],char m)
+{
+    int count=0,i;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(tolower(s[i])==m)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    char s[1000];
+    scanf("%[^\n]s",s);
+    int j,ret;
+    char vowel[5]={'a','e','i','o','u'};
+    for(j=0;j<5;j++)
+    {
+        ret=search(s,vowel[j]);
+        if(ret>0)
+        {
+            printf(" %c ---> %d ",vowel[j],ret);
+        }
+    }
+    
+    
+  
+    
+    
+
+}
+
+
