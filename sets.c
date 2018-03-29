239f509bdb1b69ea435b8e6fedad3aabb996d038
SaralaDeviCS <32431208+SaralaDeviCS@users.noreply.github.com>
2018-03-29 12:41:19 +0530

    Update sets.c

--- sets.c
+++ sets.c
@@ -14,27 +14,27 @@ int sets(int a[100],int n)
 	int c=pow(2,n);
 	int i,j,k,set[n];
 	for(i=1;i<c;i++)
-    {
+    	{
 		k=0;
 		for(j=0;j<n;j++)
-        {
+       		 {
 			if(i&1<<j)
 			{
 				set[k]=a[j];
 				k++;
 			}
-        }
+       		 }
 		print(set,k);
 	}
 }
 int main()
 {
 	int n,i,a[100];
-    printf("enter the number of elements");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    sets(a,n);
+  	printf("enter the number of elements");
+    	scanf("%d",&n);
+    	for(i=0;i<n;i++)
+    	{
+        	scanf("%d",&a[i]);
+   	}
+   	sets(a,n);
 }
